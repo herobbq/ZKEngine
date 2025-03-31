5486779ea09184a71f58b8ad229f5442cd902b43
chnezike <chenzike@zulong.com>
2025-03-31 18:57:01 +0800

    增加buffer

--- ZKEngine/RHI/D3D12Resources.cpp
+++ ZKEngine/RHI/D3D12Resources.cpp
@@ -10,3 +10,21 @@ void FD3D12Heap::Create(D3D12_HEAP_DESC& Des)
 {
     Parent->GetDevice()->CreateHeap(&Des, IID_PPV_ARGS(&Heap));
 }
+
+FD3D12Resource::FD3D12Resource(FD3D12Device* Parent, D3D12_RESOURCE_DESC& InDesc)
+    : FD3D12DeviceChild(Parent)
+    , Desc(InDesc)
+{
+}
+
+FD3D12VertexBuffer::FD3D12VertexBuffer(FD3D12Device* Parent, D3D12_RESOURCE_DESC& InDesc,D3D12_HEAP_PROPERTIES& InHeapProp)
+    :FD3D12Resource(Parent, InDesc)
+{
+    GRS_THROW_IF_FAILED(Parent->GetDevice()->CreateCommittedResource(
+                &InHeapProp,
+                D3D12_HEAP_FLAG_NONE,
+                &InDesc,
+                D3D12_RESOURCE_STATE_GENERIC_READ,
+                nullptr,
+                IID_PPV_ARGS(&Resource)));
+}
--- ZKEngine/RHI/D3D12Resources.h
+++ ZKEngine/RHI/D3D12Resources.h
@@ -10,3 +10,34 @@ public:
 protected:
     ComPtr<ID3D12Heap> Heap;
 };
+
+
+class FD3D12Resource :  public FD3D12DeviceChild
+{
+public:
+    FD3D12Resource(FD3D12Device* Parent, D3D12_RESOURCE_DESC& InDesc);
+    
+protected:
+    ComPtr<ID3D12Resource> Resource;
+    D3D12_RESOURCE_DESC Desc;
+};
+
+class FD3D12VertexBuffer : public FD3D12Resource
+{
+public:
+    FD3D12VertexBuffer(FD3D12Device* Parent, D3D12_RESOURCE_DESC& InDesc,D3D12_HEAP_PROPERTIES& InHeapProp);
+    void* GetVertexData() { return Data; }
+    inline  uint32 GetStride() const
+    {
+        return Stride;
+    }
+    inline uint32 GetNumVertices() const
+    {
+        return NumVertices;
+    }
+protected:
+    uint8* Data;
+    uint32 Stride;
+    uint32 NumVertices;
+    
+};
