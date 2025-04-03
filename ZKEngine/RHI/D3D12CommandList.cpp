0c594e07c5e02afc6bd150e440a567842b74ecbe
chnezike <chenzike@zulong.com>
2025-04-03 18:03:55 +0800

    增加Commandlist

--- ZKEngine/RHI/D3D12CommandList.cpp
+++ ZKEngine/RHI/D3D12CommandList.cpp
@@ -1,1 +1,29 @@
 ﻿#include "D3D12CommandList.h"
+#include "D3D12Queue.h"
+#include "D3D12CommandAllocator.h"
+FD3D12CommandList::FD3D12CommandList(FD3D12CommandAllocator* CommandAllocator)
+    :Device(CommandAllocator->Device)
+    ,QueueType(CommandAllocator->QueueType)
+{
+    switch (QueueType)
+    {   default:
+            break;
+        case ED3D12QueueType::Direct:
+        case ED3D12QueueType::Async:
+        GRS_THROW_IF_FAILED(Device->GetDevice()->CreateCommandList(0, GetD3DCommandListType(QueueType)
+                , *CommandAllocator, nullptr, IID_PPV_ARGS(&Interfaces.GraphicsCommandList)));
+        Interfaces.CommandList = Interfaces.GraphicsCommandList;
+        Interfaces.CommandList->QueryInterface(IID_PPV_ARGS(&Interfaces.CopyCommandList));
+        break;
+        case ED3D12QueueType::Copy:
+            GRS_THROW_IF_FAILED(Device->GetDevice()->CreateCommandList(
+            0,
+            GetD3DCommandListType(QueueType),
+            *CommandAllocator,
+            nullptr,
+            IID_PPV_ARGS(&Interfaces.CopyCommandList)
+        ));
+        Interfaces.CommandList = Interfaces.CopyCommandList;
+        break;
+    }
+}
--- ZKEngine/RHI/D3D12CommandList.h
+++ ZKEngine/RHI/D3D12CommandList.h
@@ -1,7 +1,23 @@
 ﻿#pragma once
+#include <wrl/client.h>
 
+#include "D3D12CommandAllocator.h"
+using namespace Microsoft::WRL;
+enum class ED3D12QueueType;
+class FD3D12Device;
+class FD3D12CommandAllocator;
 class FD3D12CommandList
 {
 public:
-    
+    FD3D12CommandList(FD3D12CommandAllocator* CommandAllocator);
+protected:
+    FD3D12Device*       const Device;
+    ED3D12QueueType     const QueueType;
+private:
+    struct FInterfaces
+    {
+       ComPtr<ID3D12CommandList>          CommandList;
+        ComPtr<ID3D12GraphicsCommandList>  CopyCommandList;
+        ComPtr<ID3D12GraphicsCommandList>  GraphicsCommandList;
+    }Interfaces;
 };
